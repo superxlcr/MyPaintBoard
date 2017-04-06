eba86707511ed436d267cccfb3de4198206209c6
superxlcr <405634593@qq.com>
2017-04-06 13:15:06 +0800

    add getOrderStr, add new Thread to upload file

--- app/src/main/java/com/app/superxlcr/mypaintboard/model/Protocol.java
+++ app/src/main/java/com/app/superxlcr/mypaintboard/model/Protocol.java
@@ -214,37 +214,7 @@ public class Protocol {
 		return order;
 	}
 
-	public void setOrder(int order) {
-		this.order = order;
-	}
-
-	public long getTime() {
-		return time;
-	}
-
-	public void setTime(long time) {
-		this.time = time;
-	}
-
-	public JSONArray getContent() {
-		return content;
-	}
-
-	public void setContent(JSONArray content) {
-		this.content = content;
-	}
-
-	public String getJsonStr() {
-		return jsonStr;
-	}
-
-	public void setJsonStr(String jsonStr) {
-		this.jsonStr = jsonStr;
-	}
-
-	@Override
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
+	public String getOrderStr() {
 		String orderStr = "unknow";
 		switch (order) {
 		case LOGIN:
@@ -307,9 +277,43 @@ public class Protocol {
 		default:
 			break;
 		}
-		sb.append("order :" + orderStr + "\n");
-		sb.append("time :" + time + "\n");
-		sb.append("content :" + content + "\n");
+		return orderStr;
+	}
+	
+	public void setOrder(int order) {
+		this.order = order;
+	}
+
+	public long getTime() {
+		return time;
+	}
+
+	public void setTime(long time) {
+		this.time = time;
+	}
+
+	public JSONArray getContent() {
+		return content;
+	}
+
+	public void setContent(JSONArray content) {
+		this.content = content;
+	}
+
+	public String getJsonStr() {
+		return jsonStr;
+	}
+
+	public void setJsonStr(String jsonStr) {
+		this.jsonStr = jsonStr;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("order :" + getOrderStr() + "\r\n");
+		sb.append("time :" + time + "\r\n");
+		sb.append("content :" + content + "\r\n");
 		return sb.toString();
 	}
 
--- app/src/main/java/com/app/superxlcr/mypaintboard/utils/UploadFileUtil.java
+++ app/src/main/java/com/app/superxlcr/mypaintboard/utils/UploadFileUtil.java
@@ -23,53 +23,63 @@ public class UploadFileUtil {
 
     private static final String TAG = UploadFileUtil.class.getSimpleName();
 
-    private UploadFileUtil() {}
+    private UploadFileUtil() {
+    }
 
     private static boolean flag;
 
     /**
      * 上传图片文件到服务器，自带阻塞与进度条
+     *
      * @param context 上下文
-     * @param pic 图片
+     * @param pic     图片
      */
-    public static void uploadPic(Context context, Uri pic) {
+    public static void uploadPic(final Context context, final Uri pic) {
         flag = true;
-        Dialog dialog = LoadingDialogUtils.showDialog(context, "正在上传图片...", true);
+        final Dialog dialog = LoadingDialogUtils.showDialog(context, "正在上传图片...", true);
         dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
             @Override
             public void onCancel(DialogInterface dialog) {
                 flag = false;
             }
         });
-        try {
-            InputStream is = context.getApplicationContext().getContentResolver().openInputStream(pic);
-            byte[] fileBytes = new byte[1024];
-            int len;
-            while ((len = is.read(fileBytes, 0, fileBytes.length)) > 0) {
-                if (!flag) { // 传输被终止
-                    return;
-                }
-                JSONArray content = new JSONArray();
-                // stateCode + len + file
-                content.put(Protocol.UPLOAD_PIC_CONTINUE);
-                content.put(len);
-                String str = new String(fileBytes, 0, len, "ISO-8859-1");
-                content.put(str);
+        // 子线程上传
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    InputStream is = context.getApplicationContext().getContentResolver().openInputStream(pic);
+                    byte[] fileBytes = new byte[1024];
+                    int len;
+                    while ((len = is.read(fileBytes, 0, fileBytes.length)) > 0) {
+                        if (!flag) { // 传输被终止
+                            break;
+                        }
+                        JSONArray content = new JSONArray();
+                        // stateCode + len + file
+                        content.put(Protocol.UPLOAD_PIC_CONTINUE);
+                        content.put(len);
+                        String str = new String(fileBytes, 0, len, "ISO-8859-1");
+                        content.put(str);
 //                MyLog.d(TAG, "len : " + len + "fileBytesLen : " + fileBytes.length + "strLen : " + str.getBytes("ISO-8859-1").length);
-                Protocol protocol = new Protocol(Protocol.UPLOAD_PIC, System.currentTimeMillis(), content);
-                CommunicationController.getInstance(context).sendProtocol(protocol);
+                        Protocol protocol = new Protocol(Protocol.UPLOAD_PIC, System.currentTimeMillis(), content);
+                        CommunicationController.getInstance(context).sendProtocol(protocol);
+                    }
+                    if (flag) { // 没用中止传输
+                        // 发送finish信号
+                        JSONArray content = new JSONArray();
+                        // stateCode + len + file
+                        content.put(Protocol.UPLOAD_PIC_FINISH);
+                        Protocol protocol = new Protocol(Protocol.UPLOAD_PIC, System.currentTimeMillis(), content);
+                        CommunicationController.getInstance(context).sendProtocol(protocol);
+                    }
+                } catch (IOException e) {
+                    MyLog.e(TAG, Log.getStackTraceString(e));
+                } finally {
+                    flag = false;
+                    LoadingDialogUtils.closeDialog(dialog);
+                }
             }
-            // 发送finish信号
-            JSONArray content = new JSONArray();
-            // stateCode + len + file
-            content.put(Protocol.UPLOAD_PIC_FINISH);
-            Protocol protocol = new Protocol(Protocol.UPLOAD_PIC, System.currentTimeMillis(), content);
-            CommunicationController.getInstance(context).sendProtocol(protocol);
-        } catch (IOException e) {
-            MyLog.e(TAG, Log.getStackTraceString(e));
-        } finally {
-            flag = false;
-            LoadingDialogUtils.closeDialog(dialog);
-        }
+        }).start();
     }
 }
